60c5dd7a0a1e31b25f2542299bfdfb06e0785053
amersb <535640@student.fontys.nl>
2026-09-07 10:17:08 +0200

    Initial commit

---
+++ src/main/java/nl/woningmatch/woningmatch/WoningMatchApplication.java
@@ -0,0 +1,13 @@
+package nl.woningmatch.woningmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoningMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoningMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/woningmatch/woningmatch/WoningMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.woningmatch.woningmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoningMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
